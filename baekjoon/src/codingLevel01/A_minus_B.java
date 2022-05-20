61db9c52bce38ff4abc0f6491f857aacf790257f
User <User@DESKTOP-UU1NA7G>
2022-05-20 23:11:58 +0900

    lv1_06

---
+++ baekjoon/src/codingLevel01/A_minus_B.java
@@ -0,0 +1,14 @@
+package codingLevel01;
+
+import java.util.Scanner;
+
+public class A_minus_B {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		
+		sc.close();
+		System.out.println(A-B);
+	}
+}
