5aab5362279a51d2762c86cd2a9b6244ad4958ac
User <User@DESKTOP-UU1NA7G>
2022-05-23 23:16:17 +0900

    백준 lv01_10

---
+++ baekjoon/src/codingLevel01/Joonas.java
@@ -0,0 +1,14 @@
+package codingLevel01;
+
+import java.util.Scanner;
+
+public class Joonas {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String A = sc.next();
+		
+		
+		sc.close();
+		System.out.println(A+"??!");
+	}
+}
