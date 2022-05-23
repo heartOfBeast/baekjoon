428ae19d62c154ac9279d4432e59fc8b54151685
User <User@DESKTOP-UU1NA7G>
2022-05-23 23:14:25 +0900

    백준 lv01_10

--- baekjoon/src/codingLevel01/FourFundamental.java
+++ baekjoon/src/codingLevel01/FourFundamental.java
@@ -5,14 +5,11 @@ import java.util.Scanner;
 public class FourFundamental {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int A = sc.nextInt();
-		int B = sc.nextInt();
+		String A = sc.next();
+		
 		
 		sc.close();
-		System.out.println(A+B);
-		System.out.println(A-B);
-		System.out.println(A*B);
-		System.out.println(A/B);
-		System.out.println(A%B);
+		System.out.println(A+"??!");
+
 	}
 }
